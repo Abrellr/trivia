b416860d301224cb2784da0ae67e3ee31536fe92
Abrellr <69308389+Abrellr@users.noreply.github.com>
2021-03-17 16:50:28 +0100

    add real time score count

--- src/components/PlayDiceCard.js
+++ src/components/PlayDiceCard.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Container, Row, Card, Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Card, Button, Col } from "react-bootstrap";
 import DiceOne from "./DiceOne.js";
 import DiceTwo from "./DiceTwo.js";
 import diceOne from "../data/diceOne";
@@ -13,17 +13,24 @@ export default function PlayDiceCard() {
   const [scorePlayer2, setScorePlayer2] = useState(0);
 
   function nextDiceIndex() {
-    if (diceOneIndex > diceTwoIndex) {
-      setScorePlayer1(scorePlayer1 + 1);
-    } else if (diceTwoIndex > diceOneIndex) {
-      setScorePlayer2(scorePlayer2 + 1);
-    } else if (diceTwoIndex === diceOneIndex) {
-      setScorePlayer2(scorePlayer2 + 0);
-      setScorePlayer1(scorePlayer1 + 0);
-    }
     setDiceOneIndex(Math.floor(Math.random() * 6));
     setDiceTwoIndex(Math.floor(Math.random() * 6));
   }
+
+  //setScore will be triggered each time dice Index changes
+  useEffect(() => {
+    function getScore() {
+      if (diceOneIndex > diceTwoIndex) {
+        setScorePlayer1(scorePlayer1 + 1);
+      } else if (diceTwoIndex > diceOneIndex) {
+        setScorePlayer2(scorePlayer2 + 1);
+      } else if (diceTwoIndex === diceOneIndex) {
+        setScorePlayer2(scorePlayer2 + 0);
+        setScorePlayer1(scorePlayer1 + 0);
+      }
+    }
+    getScore();
+  }, [diceOneIndex]);
 
   function setWinner() {
     if (diceOneIndex > diceTwoIndex) {
@@ -39,10 +46,6 @@ export default function PlayDiceCard() {
     <Container fluid>
       <Row className="justify-content-center">
         <h3 className="winnerText">{setWinner()}</h3>
-      </Row>
-      <Row>
-        <p>Player 1 score: {scorePlayer1}</p>
-        <p>Player 2 score: {scorePlayer2}</p>
       </Row>
       <Row className="cardRow">
         <Card className="playDiceCard">
@@ -62,12 +65,19 @@ export default function PlayDiceCard() {
         <Button
           id="nextBtn"
           onClick={() => {
-            //counter();
             nextDiceIndex();
           }}
         >
           Refresh me!
         </Button>
+      </Row>
+      <Row className="justify-content-center flex-column mt-5">
+        <Col className="d-flex justify-content-center">
+          <p className="player">Player 1 score: {scorePlayer1}</p>
+        </Col>
+        <Col className="d-flex justify-content-center">
+          <p className="player">Player 2 score: {scorePlayer2}</p>
+        </Col>
       </Row>
     </Container>
   );
